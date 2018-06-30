d8cecf082c68274483433b477ed495c0dbc88b7a
simen <simennj@outlook.com>
2018-06-30 21:53:09 +0200

    add category filter

--- src/FilterForm.vue
+++ src/FilterForm.vue
@@ -8,6 +8,13 @@
         v-model="filter.name"
         label="name"
       />
+      <v-select
+        :items="availableCategories"
+        :loading="loading"
+        v-model="filter.category"
+        label="Kategorier"
+        multiple
+      />
       <v-layout
         wrap
         v-for="range in ranges"
@@ -21,6 +28,7 @@
           shrink
         >
           <v-text-field
+            label="min"
             v-model="range.limits[0]"
             type="number"
           />
@@ -30,6 +38,7 @@
           shrink
         >
           <v-text-field
+            label="maks"
             v-model="range.limits[1]"
             type="number"
           />
@@ -49,6 +58,8 @@
   </v-container>
 </template>
 <script>
+  import { CATEGORIES } from './http-functions'
+
   export default {
     name: 'filter-form',
     props: {
@@ -61,7 +72,9 @@
           {name: 'volum', limits: [0, 20], min: 0, max: 20, step: 0.01, suffix: 'l', query: 'volume'},
           {name: 'alkohol', limits: [0, 60], min: 0, max: 60, suffix: '%', query: 'alcohol'},
           {name: 'alkoholpris', limits: [0, 20000], min: 0, max: 20000, suffix: 'kr/cl', query: 'alcohol_price'}
-        ]
+        ],
+        loading: true,
+        availableCategories: []
       }
     },
     watch: {
@@ -73,6 +86,25 @@
           }
         },
         deep: true
+      }
+    },
+    mounted () {
+      this.getCategories()
+    },
+    methods: {
+      getCategories () {
+        this.loading = true
+        return CATEGORIES.get('/')
+          .then(response => {
+            this.availableCategories = response.data.results.map(category => {
+              return {value: category.id, text: category.name}
+            })
+            this.loading = false
+          })
+          .catch(e => {
+            console.log(e)
+            this.loading = false
+          })
       }
     }
   }

112cd639e139656af940bccca70be1ba876bc83e
simen <simennj@outlook.com>
2018-06-30 21:51:30 +0200

    add http function for retrieving categories

--- src/http-functions.js
+++ src/http-functions.js
@@ -1,5 +1,11 @@
 import axios from 'axios'
 
+export const API_URL = 'http://127.0.0.1:8000'
+
 export const PRODUCTS = axios.create({
-  baseURL: 'http://127.0.0.1:8000/products'
+  baseURL: API_URL + '/products'
+})
+
+export const CATEGORIES = axios.create({
+  baseURL: API_URL + '/category'
 })

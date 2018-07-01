19936afde5ccc30d00548d85728b059079a68203
simen <simennj@outlook.com>
2018-07-01 19:29:57 +0200

    add norwegian locale

---
+++ src/locale/no.js
@@ -0,0 +1,14 @@
+export default {
+  dataIterator: {
+    rowsPerPageText: 'Rader per side:',
+    rowsPerPageAll: 'Alle',
+    pageText: '{0}-{1} av {2}',
+    noResultsText: 'Ingen treff',
+    nextPage: 'Neste side',
+    prevPage: 'Forrige side'
+  },
+  dataTable: {
+    rowsPerPageText: 'Rader per side:'
+  },
+  noDataText: 'Ingen data tilgjengelig'
+}

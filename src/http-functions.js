da9fbb17ae488fc5d879f3c0d12acd07d76eb925
simen <simennj@outlook.com>
2018-07-07 09:53:18 +0200

    use location href to determine api url

--- src/http-functions.js
+++ src/http-functions.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-export const API_URL = 'http://127.0.0.1:8000'
+let apiUrl = '/api'
+if (/https?:\/\/[^/:]*:8080/i.test(window.location.href)) {
+  apiUrl = /(https?:\/\/[^/:]*:)8080/i.exec(window.location.href)[1] + '8000'
+}
+export const API_URL = apiUrl
 
 export const PRODUCTS = axios.create({
   baseURL: API_URL + '/products'

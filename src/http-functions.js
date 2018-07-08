1263345d3173f7be29e72231d1554adab2981b2e
simen <simennj@outlook.com>
2018-07-08 22:13:00 +0200

    add http function for getting product aggregations

--- src/http-functions.js
+++ src/http-functions.js
@@ -10,6 +10,10 @@ export const PRODUCTS = axios.create({
   baseURL: API_URL + '/products'
 })
 
+export const PRODUCT_AGGREGATIONS = axios.create({
+  baseURL: API_URL + '/products/aggregations'
+})
+
 export const CATEGORIES = axios.create({
   baseURL: API_URL + '/category'
 })

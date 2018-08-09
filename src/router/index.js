4a7ac326e218c8a10e509eac52c0a5bc16470c7a
simen <simennj@outlook.com>
2018-08-09 14:29:33 +0200

    remove HelloWorld component

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Table from '@/components/Table'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/h',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/',
       name: 'Table',
